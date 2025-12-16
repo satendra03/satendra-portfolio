37b6e451a3bc5356471ffa0be17f06331fa8bf5e
satendra03 <satendrakumarparteti.work@gmail.com>
2025-12-17 02:04:00 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  // @ts-expect-error -- eslint config is valid in next.config.js but missing in NextConfig type for this version
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
